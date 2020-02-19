820d3f8bd67c89e2bd5bd6d1234fc7d05a3a4e16
Isaac Rosenberg <irosenb7@gmail.com>
2020-02-19 14:44:25 -0800

    Detail page, navigation stack

--- App.js
+++ App.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import Home from './app/screens/Home'
+import Detail from './app/screens/Detail'
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <Home/>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="Detail" component={Detail} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
